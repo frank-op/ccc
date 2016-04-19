8e31da92d03f8f5f7abcd0b39c771eb81a9f2c87
Frank Oppelt <frank.oppelt@cgm.com>
2016-04-19 16:14:59 +0200

    intermediate commit before I go home

--- ccc/src/main/java/ccc/addictiveGame/execute/Executor.java
+++ ccc/src/main/java/ccc/addictiveGame/execute/Executor.java
@@ -3,6 +3,7 @@ package ccc.addictiveGame.execute;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import ccc.addictiveGame.field.Board;
 import ccc.addictiveGame.field.Cell;
@@ -13,7 +14,7 @@ public class Executor {
 
 		String[] inputArray = input.split(" ");
 		Board board = createBoard(inputArray[0], inputArray[1]);
-		List<Integer> valuesToFind = getValuesToFind(inputArray);
+		List<Integer> valuesToFind = getValuesToFindForLevel_1_Input(inputArray);
 		List<Cell> cellsForValues = getCellsForValues(board, valuesToFind);
 		return OutputFormatter.getFormattedContent(cellsForValues);
 	}
@@ -22,12 +23,32 @@ public class Executor {
 		return new Board(Integer.valueOf(rows), Integer.valueOf(cols));
 	}
 
-	private static List<Integer> getValuesToFind(String[] inputArray) {
-		return Arrays.asList(inputArray).subList(3, inputArray.length).stream().map(x -> Integer.valueOf(x))
-				.collect(Collectors.toList());
+	private static List<Integer> getValuesToFindForLevel_1_Input(String[] inputArray) {
+		List<String> valuesFromInput = getValuesFromInput(inputArray);
+		return valuesFromInput.stream().map(x -> Integer.valueOf(x)).collect(Collectors.toList());
+	}
+
+	private static List<String> getValuesFromInput(String[] inputArray) {
+		return Arrays.asList(inputArray).subList(3, inputArray.length);
 	}
 
 	private static List<Cell> getCellsForValues(Board board, List<Integer> valuesToFind) {
 		return valuesToFind.stream().map(x -> board.findCellByValue(x)).collect(Collectors.toList());
+	}
+
+	public static String executeFindManhattenDistance(String input) {
+
+		String[] inputArray = input.split(" ");
+		Board board = createBoard(inputArray[0], inputArray[1]);
+
+		return null;
+	}
+
+	private static List<Integer> getValuesToFindForLevel_2_Input(String[] inputArray) {
+		List<String> valuesFromInput = getValuesFromInput(inputArray);
+
+		IntStream.range(0, valuesFromInput.size()).filter(n -> n % 2 == 0).mapToObj(valuesFromInput::get)
+				.collect(Collectors.toList());
+		return null;
 	}
 }
